cd2b928c647a44997986d3a8b64fbe364ca9a4a1
rixong <rixong@gmail.com>
2020-05-08 16:40:49 -0500

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,74 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import {
+  addCurUser,
+  deleteCurUser,
+  addCurrentAnnualReport,
+  addCabins,
+  addUsers,
+  getReservations,
+  addRepairTickets,
+  deleteAll
+} from '../actions/index';
+
 class Home extends Component {
+
+  componentDidMount() {
+    this.fetchInitialData()
+  }
+
+  fetchInitialData = () => {
+    if (localStorage.getItem('accessToken')) {
+      Promise.all([
+        fetch("http://localhost:3000/api/v1/annual_report/current", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer: ${localStorage.getItem('accessToken')}`
+          }
+        }),
+
+        fetch("http://localhost:3000/api/v1/users", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer: ${localStorage.getItem('accessToken')}`
+          }
+        }),
+
+        fetch("http://localhost:3000/api/v1/cabins", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer: ${localStorage.getItem('accessToken')}`
+          }
+        }),
+
+        fetch("http://localhost:3000/api/v1/reservations", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer: ${localStorage.getItem('accessToken')}`
+          }
+        }),
+
+        fetch("http://localhost:3000/api/v1/repairs", {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer: ${localStorage.getItem('accessToken')}`
+          }
+        }),
+
+      ]).then(([res1, res2, res3, res4, res5]) => {
+        return Promise.all([res1.json(), res2.json(), res3.json(), res4.json(), res5.json()])
+      }).then(([res1, res2, res3, res4, res5]) => {
+        this.props.addCurrentAnnualReport(res1);
+        this.props.addUsers(res2)
+        this.props.addCabins(res3);
+        this.props.getReservations(res4);
+        this.props.addRepairTickets(res5);
+      })
+    }
+  }
+
+
   render() {
     return (
       <div className="ui container home" id="home">
@@ -36,4 +103,13 @@ const mapStateToProps = state => {
   }
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, {
+  addCurUser,
+  deleteCurUser,
+  addCurrentAnnualReport,
+  addCabins,
+  addUsers,
+  getReservations,
+  addRepairTickets,
+  deleteAll
+})(Home);
