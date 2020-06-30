1f9b331a1b3ad0aeacfafe2c3f66b7512c5121f1
rixong <rixong@gmail.com>
2020-06-30 17:23:03 -0400

    refactored tide calcs

--- src/components/main/Tides.js
+++ src/components/main/Tides.js
@@ -3,14 +3,13 @@ import moment from 'moment';
 import axios from 'axios';
 
 const URL = 'https://tidesandcurrents.noaa.gov/api/datagetter';
-//begin_date, end_date  
 const otherParams = 'station=8413320&product=predictions&datum=MLW&time_zone=lst_ldt&units=english&format=json'
 const curDay = moment(Date.now()).format('YYYYMMDD').toString();
 const nextDay = moment(Date.now()).add(1, 'd').format('YYYYMMDD').toString();
 class Tides extends Component {
 
   state = {
-    tides: null
+    tides: []
   }
 
   componentDidMount() {
@@ -24,68 +23,78 @@ class Tides extends Component {
       })
   }
 
-  calculateMaxMinTides = (parsedData) => {
-    parsedData.forEach(entry => entry.v = parseFloat(entry.v));
-    let rising = parsedData[0].v < parsedData[1].v;
-    const tides = [];
-    for (let j = 1; j < parsedData.length; j++) {
-      if (rising && (parsedData[j - 1].v) > parsedData[j].v) {
-        parsedData[j - 1].type = 'High';
-        parsedData[j - 1].time = parsedData[j - 1].t.substring(11);
-        parsedData[j - 1].date = parsedData[j - 1].t.substring(0,10);
-        tides.push(parsedData[j - 1])
-        rising = !rising;
+  calculateMaxMinTides = (data) => {    
+    data.forEach(entry => entry.v = parseFloat(entry.v));
+    this.setState({ rising: data[0].v < data[1].v });
+    for (let j = 1; j < data.length; j++) {
+      if (this.state.rising && (data[j - 1].v) > data[j].v) {
+        this.addTide(data[j - 1], 'High')
       }
+      if (!this.state.rising && data[j - 1].v < data[j].v) {
+        this.addTide(data[j - 1], 'Low')
+      }
+    }
+  }
 
-      if (!rising && parsedData[j - 1].v < parsedData[j].v) {
-        parsedData[j - 1].type = 'Low';
-        parsedData[j - 1].time = parsedData[j - 1].t.substring(11);
-        parsedData[j - 1].date = parsedData[j - 1].t.substring(0,10);
-        tides.push(parsedData[j - 1])
-        rising = !rising;
-      }
-    }    
-    this.setState({ tides })
+  addTide = (tide, type) => {
+    tide.type = type;
+    tide.time = tide.t.substring(11);
+    tide.date = tide.t.substring(0, 10);    
+    this.state.tides.push(tide)
+    this.setState({ rising: !this.state.rising })
   }
 
   todaysTides = () => {
     if (this.state.tides) {
-    return this.state.tides.filter((tide) => tide.date === this.state.tides[0].date)
-      .map(tide => {
-        let depth = tide.v.toFixed(1);
-        return <div key={tide.t}> {tide.time}  {tide.type} tide ({depth}')  </div>
-      })
+      return this.state.tides.filter((tide) => tide.date === this.state.tides[0].date)
+        .map(tide => {
+          let depth = tide.v.toFixed(1);
+          return <div key={tide.t}> {tide.time}  {tide.type} tide ({depth}')  </div>
+        })
     }
   }
 
   tomorrowsTides = () => {
     if (this.state.tides) {
-    return this.state.tides.filter((tide) => tide.date !== this.state.tides[0].date)
-      .map(tide => {
-        let depth = tide.v.toFixed(1);
-        return <div key={tide.t}> {tide.time}  {tide.type} tide ({depth}')  </div>
-      })
+      return this.state.tides.filter((tide) => tide.date !== this.state.tides[0].date)
+        .map(tide => {
+          let depth = tide.v.toFixed(1);
+          return <div key={tide.t}> {tide.time}  {tide.type} tide ({depth}')  </div>
+        })
     }
   }
 
+  renderView = () => {
+    if (this.state.loading) {
+      return <p>Loading tides...</p>
+    }
+    if (this.state.error) {
+      return <p>Tides currently unavailable</p>
+    } else {
+      return (
+        <div className="ui two column row">
+          <div className="ui eight wide column">
+            <h4 className="h4">Today's tides</h4>
+            {this.todaysTides()}
+          </div>
+          <div className="ui eight wide column">
+            <h4 className="h4">Tomorrow's tides</h4>
+            {this.tomorrowsTides()}
+          </div>
+        </div>
+      )
+    }
+  }
 
   render() {
     return (
-      <div className='tide-display'>
+      <div id='tide-display'>
         <div className="ui grid">
-          <div className="ui two column row">
-            <div className="ui eight wide column">
-              <h4>Today's tides</h4>
-              {this.todaysTides()}
-            </div>
-            <div className="ui eight wide column">
-              <h4>Tomorrow's tides</h4>
-              {this.tomorrowsTides()}
-            </div>
-          </div>
+          {this.renderView()}
         </div>
       </div>
     )
   }
 }
+
 export default Tides;
