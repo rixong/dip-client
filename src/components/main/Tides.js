36de6382f5aced7075b84257eb287aff3477605d
rixong <rixong@gmail.com>
2020-07-02 11:26:41 -0400

    done

--- src/components/main/Tides.js
+++ src/components/main/Tides.js
@@ -10,54 +10,67 @@ const curDay = moment(Date.now()).format('YYYYMMDD').toString();
 // const nextDay = moment(Date.now()).add(1, 'd').format('YYYYMMDD').toString();
 class Tides extends Component {
 
-  state = {
-    tides: []
+constructor(props) {
+  super(props)
+  this.state = {
+    tides: [],
+    rising: 'dog'
   }
+}
 
   componentDidMount() {
-    if(this.props.tides){
-      this.calculateMaxMinTides()
-    }
-  }
 
-  calculateMaxMinTides = () => {
-    const data = [...this.props.tides]
-    console.log(data);
-    
-    if (data){
-      data.forEach(entry => entry.v = parseFloat(entry.v));
-      console.log(data[0].v < data[1].v);
-    this.setState({rising: false});  ///false
-    console.log('rising',this.state.rising);
-    
-    for (let j = 1; j < data.length; j++) {
-      if (this.state.rising && (data[j - 1].v) > data[j].v) {
-        this.addHighLowTide(data[j - 1], 'High')
-      }
-      if (!this.state.rising && data[j - 1].v < data[j].v) {
-        this.addHighLowTide(data[j - 1], 'Low')
-      }
-    }
-  }
-    // console.log(this.state.tides);
+    // if(this.props.tides){
+    //   this.calculateMaxMinTides()
+    // }
+    console.log('here', this.state.rising)
 
   }
 
-  addHighLowTide = (tide, type) => {
-    let day = ''
-    tide.t.substring(8, 10) === curDay.substring(6, 8) ? day = 'today' : day = 'tomorrow';
-    this.state.tides.push({
-      type: type,
-      time: tide.t.substring(11),
-      day,
-      v: tide.v
-    });
-    this.setState({ rising: !this.state.rising })
-    // console.log('rising',this.state.rising);
-    
+  test = () => {
+    this.setState({rising: 'cat'})
+
   }
+
+  // calculateMaxMinTides = () => {
+  //   const data = [...this.props.tides]
+  //   console.log(data);
+    
+  //   if (data){
+  //     data.forEach(entry => entry.v = parseFloat(entry.v));
+  //     console.log(data[0].v < data[1].v);
+  //     this.setState({ rising: 'pig' });  ///false
+  //   console.log('rising',this.state.rising);
+    
+  //   for (let j = 1; j < data.length; j++) {
+  //     if (this.state.rising && (data[j - 1].v) > data[j].v) {
+  //       this.addHighLowTide(data[j - 1], 'High')
+  //     }
+  //     if (!this.state.rising && data[j - 1].v < data[j].v) {
+  //       this.addHighLowTide(data[j - 1], 'Low')
+  //     }
+  //   }
+  // }
+  //   // console.log(this.state.tides);
+
+  // }
+
+  // addHighLowTide = (tide, type) => {
+  //   let day = ''
+  //   tide.t.substring(8, 10) === curDay.substring(6, 8) ? day = 'today' : day = 'tomorrow';
+  //   this.state.tides.push({
+  //     type: type,
+  //     time: tide.t.substring(11),
+  //     day,
+  //     v: tide.v
+  //   });
+  //   this.setState({ rising: !this.state.rising })
+  //   // console.log('rising',this.state.rising);
+    
+  // }
 
   render() {
+
     if (this.state.loading) {
       return <p>Loading tides...</p>
     }
@@ -69,10 +82,11 @@ class Tides extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    tides: state.externalApis.tides
-  }
-};
+// const mapStateToProps = state => {
+//   return {
+//     tides: state.externalApis.tides
+//   }
+// };
 
-export default connect(mapStateToProps)(Tides);
+// export default connect(mapStateToProps)(Tides);
+export default Tides;
